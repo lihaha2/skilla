d239941f67890778a1dfe7a2ff9534ca9d942b62
Likhachev Mikhail <lihahasheshki@gmail.com>
2022-04-14 13:59:52 +0300

    fix

--- src/components/Calls/Record.tsx
+++ src/components/Calls/Record.tsx
@@ -6,22 +6,22 @@ const Record = ({record, partnership_id}:{record: string, partnership_id: string
 
     useEffect(()=>{
         (async()=>{
-            // if(!!record){
-            //     try {
-            //         const {data: audio} = await axios.post(`https://api.skilla.ru/mango/getRecord?record=${record}&partnership_id=${partnership_id}`,{},{
-            //             headers: {
-            //                 Authorization: `Bearer qwerty123`,
-            //                 'Content-type': 'audio/mpeg; audio/x-mpeg; audio/x-mpeg-3; audio/mpeg3',
-            //                 'Content-Transfer-Encoding': 'binary',
-            //                 'Content-Disposition': 'filename="record.mp3"',
-            //             },
-            //         })
-            //         console.log('audio', audio)
-            //         setApiRecord(audio)
-            //     } catch (error) {
-            //         console.log('Record',error)
-            //     }
-            // }
+            if(!!record){
+                try {
+                    const {data: audio} = await axios.post(`https://api.skilla.ru/mango/getRecord?record=${record}&partnership_id=${partnership_id}`,{},{
+                        headers: {
+                            Authorization: `Bearer qwerty123`,
+                            'Content-type': 'audio/mpeg; audio/x-mpeg; audio/x-mpeg-3; audio/mpeg3',
+                            'Content-Transfer-Encoding': 'binary',
+                            'Content-Disposition': 'filename="record.mp3"',
+                        },
+                    })
+                    console.log('audio', audio)
+                    setApiRecord(audio)
+                } catch (error) {
+                    console.log('Record',error)
+                }
+            }
         })()
     },[record, partnership_id])
 
